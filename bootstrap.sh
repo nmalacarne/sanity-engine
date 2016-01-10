af9ba93938320358860ede8b0e35f38a948007b9
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2016-01-10 18:19:04 -0500

    vagrant bootstrap now apt-get quiet; vagrant bootstrap now sets default node version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt-get update -q
-sudo apt-get install -y curl
+sudo apt-get install -qy curl
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 source ~/.nvm/nvm.sh
 cd /vagrant
 nvm install
+nvm alias default
 npm install
