c37c7f0e82b1be7674a6c6a603de65004a7642bd
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2016-01-10 01:37:50 -0500

    updated pool test

--- test/pool.js
+++ test/pool.js
@@ -4,21 +4,21 @@ const expect  = require('expect.js');
 
 const engine = require(process.cwd());
 
-const pool  = engine.pool;
-const def   = engine.defaults;
+const pool      = engine.pool;
+const defaults  = engine.defaults;
 
 describe('pool', function () {
   describe('#constructor()', function () {
     it('should initialize with default values', function () {
-      var p = pool();
+      const p = pool();
 
-      expect(p.min).to.be(def.pool.min);
-      expect(p.max).to.be(def.pool.max);
-      expect(p.val).to.be(def.pool.max);
+      expect(p.min).to.be(defaults.pool.min);
+      expect(p.max).to.be(defaults.pool.max);
+      expect(p.val).to.be(defaults.pool.max);
     });
 
     it('should initialize with custom values', function () {
-      var p = pool({min: 10, max: 20, val: 15});
+      const p = pool({min: 10, max: 20, val: 15});
 
       expect(p.min).to.be(10);
       expect(p.max).to.be(20);
@@ -26,27 +26,27 @@ describe('pool', function () {
     });
 
     it('should allow starting value to equal MIN', function () {
-      var p = pool({val: def.pool.min});
+      const p = pool({val: defaults.pool.min});
 
-      expect(p.val).to.be(def.pool.min);
+      expect(p.val).to.be(defaults.pool.min);
     });
 
-    it('should set starting value no lower than MIN', function () {
-      var p = pool({val: -5});
+    it('should set starting value equal to MIN when lower than MIN', function () {
+      const p = pool({val: -5});
 
-      expect(p.val).to.be(def.pool.min);
+      expect(p.val).to.be(defaults.pool.min);
     });
 
-    it('should set starting value no higher than MAX', function () {
-      var p = pool({val: 125});
+    it('should set starting value equal to MAX when higher than MAX', function () {
+      const p = pool({val: 125});
 
-      expect(p.val).to.be(def.pool.max);
+      expect(p.val).to.be(defaults.pool.max);
     });
   });
 
   describe('.val', function () {
     it('should increment by X', function () {
-      var p = pool({val: 50});
+      const p = pool({val: 50});
 
       p.val += 1;
 
@@ -54,7 +54,7 @@ describe('pool', function () {
     });
 
     it('should decrement by X', function () {
-      var p = pool();
+      const p = pool();
 
       p.val -= 1;
 
@@ -62,7 +62,7 @@ describe('pool', function () {
     });
 
     it('should set val equal to X', function () {
-      var p = pool();
+      const p = pool();
 
       p.val = 50;
 
