409f0576c6e419fbe5da6c7171ee8c341b129f6c
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2016-01-10 01:42:19 -0500

    apt-get now run witht quiet flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+sudo apt-get update -q
 sudo apt-get install -y curl
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 source ~/.nvm/nvm.sh
