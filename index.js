f38b9d34f37da56df318872cdcf17eaba3d3c4af
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2016-01-16 23:20:50 -0500

    Export helpers module from index
    In order to test the helper module, we need a way to access it
    from the engine module in tests.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.pool      = require('./lib/pool');
 exports.actor     = require('./lib/actor');
 exports.skill     = require('./lib/skill');
 exports.defaults  = require('./lib/defaults');
+exports.helper    = require('./lib/helper');
