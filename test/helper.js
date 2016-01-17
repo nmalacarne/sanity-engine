20434a2a2811230285c2166c86e6eab11886aa7b
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2016-01-17 01:30:01 -0500

    Expect Helper#limit to return an object
    The 'limit' function from the 'helper' module should return an
    object. This will allow chaining methods for easier readability.

--- test/helper.js
+++ test/helper.js
@@ -10,12 +10,22 @@ const engine  = require(process.cwd());
 const help  = engine.helper;
 
 describe('helper', function () {
-  describe('#constructor()', function () {
-    it('should return a singleton', function () {
-      const a = help;
-      const b = help;
+  describe('#limit', function () {
+    it('should return a limit object', function () {
+      const x = 1;
+      const limit = help.limit(x);
 
-      expect(a == b).to.be(true);
+      expect(limit).to.be.an('object');
+    });
+
+    it('should limit X within Y..Z', function () {
+      const x = 11;
+      const y = 1;
+      const z = 10;
+
+      const v = help.limit(x).within(y, z);
+
+      expect(v).to.be(z);
     });
   });
 });
