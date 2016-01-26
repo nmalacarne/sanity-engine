6a09324799573d534bb11dd33f6c8953efa4c069
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2016-01-26 17:21:16 -0500

    Add test coverage for helper#createMany
    Added test coverage for the 'createMany' function from the helper
    module.

--- test/helper.js
+++ test/helper.js
@@ -8,6 +8,7 @@ const engine  = require(process.cwd());
 
 // engine modules
 const help  = engine.helper;
+const pool  = engine.pool;
 
 describe('helper', function () {
   describe('#limit', function () {
@@ -43,6 +44,24 @@ describe('helper', function () {
       const def = help.define(value).or.use(1);
 
       expect(def).to.be(1);
+    });
+  });
+
+  describe('#createMany', function () {
+    it('should populate an object by running a module on a spec', function () {
+      const spec = {
+        first: {
+          val: 22
+        },
+        second: {
+          val: 76
+        }
+      };
+
+      const pools = help.createMany(pool).from(spec);
+
+      expect(pools).to.have.keys('first', 'second');
+      expect(pools.first).to.be.a(pool().constructor);
     });
   });
 });
